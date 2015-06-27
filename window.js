28c921ebda02703a75d981251419fbe93972131c
Andrew Steele <andrew@andrewsteele.me.uk>
2015-06-27 20:25:30 +0100

    Addition of Window demonstration

---
+++ window.js
@@ -0,0 +1,13 @@
+#!/usr/bin/gjs
+
+const Gtk = imports.gi.Gtk;
+
+Gtk.init(null);
+
+let window = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
+window.set_title("Window");
+window.connect("destroy", Gtk.main_quit);
+
+window.show_all();
+
+Gtk.main();
