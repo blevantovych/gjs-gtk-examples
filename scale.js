186790cc62cd4a9aab3279be700fd6d087821a46
Andrew Steele <andrew@andrewsteele.me.uk>
2015-07-07 15:59:41 +0100

    Include Scale and SpinButton widgets

---
+++ scale.js
@@ -0,0 +1,25 @@
+#!/usr/bin/gjs
+
+const Gtk = imports.gi.Gtk;
+
+Gtk.init(null);
+
+function on_scale_changed(scale)
+{
+    print(scale.get_value());
+}
+
+let window = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
+window.set_title("Scale");
+window.set_default_size(200, -1);
+window.connect("destroy", Gtk.main_quit);
+
+let scale = new Gtk.Scale();
+scale.set_range(0, 10);
+scale.set_value(7);
+scale.connect("value-changed", function() {on_scale_changed(scale)});
+window.add(scale);
+
+window.show_all();
+
+Gtk.main();
---
+++ spinbutton.js
@@ -0,0 +1,25 @@
+#!/usr/bin/gjs
+
+const Gtk = imports.gi.Gtk;
+
+Gtk.init(null);
+
+function on_spin_button_changed(spinbutton)
+{
+    print(spinbutton.get_value_as_int());
+}
+
+let window = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
+window.set_title("SpinButton");
+window.connect("destroy", Gtk.main_quit);
+
+let spinbutton = new Gtk.SpinButton();
+spinbutton.set_range(0, 10);
+spinbutton.set_increments(1, 10);
+spinbutton.set_value(3);
+spinbutton.connect("value-changed", function() {on_spin_button_changed(spinbutton)});
+window.add(spinbutton);
+
+window.show_all();
+
+Gtk.main();
