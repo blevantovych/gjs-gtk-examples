1cc513ea23259330d85a3798395b7278fd9dbbc5
Andrew Steele <andrew@andrewsteele.me.uk>
2015-08-30 06:34:00 +0100

    Add LinkButton widget example

---
+++ linkbutton.js
@@ -0,0 +1,18 @@
+#!/usr/bin/gjs
+
+const Gtk = imports.gi.Gtk;
+
+Gtk.init(null);
+
+let window = new Gtk.Window({type: Gtk.WindowType.TOPLEVEL});
+window.set_title("LinkButton");
+window.set_default_size(200, 200);
+window.connect("destroy", Gtk.main_quit);
+
+let linkbutton = new Gtk.LinkButton({label: "Programmica"});
+linkbutton.set_uri("http://programmica.com/");
+window.add(linkbutton);
+
+window.show_all();
+
+Gtk.main();
