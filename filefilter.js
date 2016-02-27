232e4784f8c20cf53755cbbf13f5645a35c9760c
Andrew Steele <andrew@andrewsteele.me.uk>
2016-02-27 22:36:17 +0000

    Add demonstration of FileFilter usage

---
+++ filefilter.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env gjs
+
+const Gtk = imports.gi.Gtk;
+
+Gtk.init(null);
+
+let filechooserdialog = new Gtk.FileChooserDialog();
+filechooserdialog.set_title("FileFilter");
+filechooserdialog.add_button("_Cancel", Gtk.ResponseType.CANCEL);
+filechooserdialog.add_button("_Open", Gtk.ResponseType.OK);
+
+let filefilter = new Gtk.FileFilter();
+filefilter.set_name("All Items");
+filefilter.add_pattern("*");
+filechooserdialog.add_filter(filefilter);
+
+let filefilter = new Gtk.FileFilter();
+filefilter.set_name("Audio")
+filefilter.add_mime_type("audio/flac")
+filefilter.add_mime_type("audio/ogg")
+filechooserdialog.add_filter(filefilter)
+
+let filefilter = new Gtk.FileFilter();
+filefilter.set_name("Images")
+filefilter.add_pattern("*.png")
+filefilter.add_pattern("*.jpg")
+filefilter.add_pattern("*.bmp")
+filechooserdialog.add_filter(filefilter)
+
+if (filechooserdialog.run() == Gtk.ResponseType.OK)
+{
+    print("File set to " + filechooserdialog.get_filename());
+}
+
+filechooserdialog.destroy();
