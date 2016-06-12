cf8d12ab0eb29f9cf9c9d8c1b59798b7dab553d3
Andrew Steele <andrew@andrewsteele.me.uk>
2016-06-12 18:14:53 +0100

    Small improvement to Notebook example Addition of TextView and associated objects

--- notebook.js
+++ notebook.js
@@ -5,24 +5,19 @@ const Gtk = imports.gi.Gtk;
 Gtk.init(null);
 
 var window = new Gtk.Window();
-window.set_title("Notebook");
+window.set_title('Notebook');
 window.set_default_size(200, 200);
-window.connect("destroy", Gtk.main_quit);
+window.connect('destroy', Gtk.main_quit);
 
 var notebook = new Gtk.Notebook();
 window.add(notebook);
 
-var label1child = new Gtk.Label({label: "Page 1 Child Widget"});
-var label1title = new Gtk.Label({label: "Page 1"});
-notebook.append_page(label1child, label1title);
-
-var label2child = new Gtk.Label({label: "Page 2 Child Widget"});
-var label2title = new Gtk.Label({label: "Page 2"});
-notebook.append_page(label2child, label2title);
-
-var label3child = new Gtk.Label({label: "Page 3 Child Widget"});
-var label3title = new Gtk.Label({label: "Page 3"});
-notebook.append_page(label3child, label3title);
+for (var i=1; i<6; i++)
+{
+    var child = new Gtk.Label({label: 'Page ' + i + ' Child Widget'});
+    var title = new Gtk.Label({label: 'Page ' + i});
+    notebook.append_page(child, title);
+}
 
 window.show_all();
 
